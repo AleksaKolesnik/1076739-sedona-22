5071f07806992d61ccfff3348bf5b56ffe0273ac
Саша <aleksa_kolesnik@mail.ru>
2021-05-25 00:05:36 +0500

    правки по замечаниям

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,11 @@ var mainNavBurger = mainNav.querySelector(".main-nav__burger");
 mainNav.classList.remove("main-nav--nojs");
 
 mainNavBurger.addEventListener("click", function() {
-    if(mainNav.classList.contains("main-nav--close")){
-        mainNav.classList.remove("main-nav--close");
-        mainNav.classList.add("main-nav--active");
-    } else {
-        mainNav.classList.remove("main-nav--active");
-        mainNav.classList.add("main-nav--close");
-    }
+  if(mainNav.classList.contains("main-nav--close")){
+    mainNav.classList.remove("main-nav--close");
+    mainNav.classList.add("main-nav--active");
+  } else {
+    mainNav.classList.remove("main-nav--active");
+    mainNav.classList.add("main-nav--close");
+  }
 });
