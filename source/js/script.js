6ab021effa69fe1eb13cb12636b39fb737cc15b6
Саша <aleksa_kolesnik@mail.ru>
2021-05-24 10:55:38 +0500

    правки допуск

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
-var mainNav = document.querySelector('.main-nav');
-var mainNavBurger = mainNav.querySelector('.main-nav__burger');
+var mainNav = document.querySelector(".main-nav");
+var mainNavBurger = mainNav.querySelector(".main-nav__burger");
 
-mainNav.classList.remove('main-nav--nojs');
+mainNav.classList.remove("main-nav--nojs");
 
-mainNavBurger.addEventListener('click', function() {
-    if(mainNav.classList.contains('main-nav--close')){
-        mainNav.classList.remove('main-nav--close');
-        mainNav.classList.add('main-nav--active');
+mainNavBurger.addEventListener("click", function() {
+    if(mainNav.classList.contains("main-nav--close")){
+        mainNav.classList.remove("main-nav--close");
+        mainNav.classList.add("main-nav--active");
     } else {
-        mainNav.classList.remove('main-nav--active');
-        mainNav.classList.add('main-nav--close');
+        mainNav.classList.remove("main-nav--active");
+        mainNav.classList.add("main-nav--close");
     }
 });
